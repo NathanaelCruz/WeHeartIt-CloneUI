c389668a2580ef65023453281139e4aa9cc591bf
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2021-02-16 20:55:57 -0300

    Structure Inicial finish with config JSON-SERVER

---
+++ weheartit-cloneui/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>We Heart It</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
---
+++ weheartit-cloneui/src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: 'red',
+    text: 'blue',
+    primary: '#1e1e45'
+  }
+}
+
+export default theme
